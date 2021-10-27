e5cf00c56c9b5a5ef0598fe31f97d484ac9d49d3
Dmitrii Nashtykov <dmitrii-nashtykov@ya.ru>
2021-10-27 12:38:46 +0300

    Промежуточный коммит для сохранения результатов

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 const app = express();
-app.get("*", express.static("./src"));
+app.get("*", express.static("./public"));
 
 const port = process.env.PORT || 8001;
 
 app.listen(port, () => {
-	console.log(`App start on port ${port}`);
+    console.log(`App start on port ${port}`);
 });
 
