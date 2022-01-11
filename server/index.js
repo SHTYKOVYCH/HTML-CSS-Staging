9d0487b989b160e126c95000fd26f9c9aee4c42a
Dmitrii Nashtykov <dmitrii-nashtykov@ya.ru>
2022-01-11 14:42:23 +0300

    HTML-CSS-learning-3 / dev / tricks     - Исправленый трикс

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 app.get("*", express.static("./public"));
+app.get("/yandex/article.json", express.static('/public/yandex/article.json'));
 
 const port = process.env.PORT || 8001;
 
